89505fed1e44915fd5be6458af3573f8014e3415
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-29 22:16:31 -0500

    finished trash drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,8 @@ var auditTask = function(taskEl)
   {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  console.log(taskEl);
   
 };
 
@@ -74,15 +76,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     console.log(event);
   },
   update: function() {
@@ -126,12 +134,14 @@ $("#trash").droppable({
   drop: function(event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
-
+    
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass(bottom-trash-active);
     console.log(ui);
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass(bottom-trash-active);
     console.log(ui);
   }
 });
@@ -272,6 +282,12 @@ $("#modalDueDate").datepicker(
     minDate: 1
   }
 );
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  })
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
